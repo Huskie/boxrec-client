1ba14e603fbbe8843a6ca1ed5c3be0e8d1d845d9
Darren Huskie <darren@darrenhuskie.com>
2017-09-18 00:25:18 +0100

    Admin updates and changed module export to client object

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
-var cheerio = require('cheerio');
-var request = require('request');
+const cheerio = require('cheerio');
+const request = require('request');
 
-var getBoxerById = function (id) {
+// The module returns a client object
+const boxrecClient = {
 
-    // Parameter validation
-    if (typeof id !== 'string') {
-        return false;
-    }
+    getBoxerById: id => {
 
-    request('http://boxrec.com/en/boxer/' + id, function (error, response, body) {
-        //console.log('error:', error);
-        //console.log('statusCode:', response && response.statusCode);
-        //console.log('body:', body);
+        // Parameter validation
+        if (typeof id !== 'string') {
+            return false;
+        }
 
-        // Load the page HTML into Cheerio
-        var $ = cheerio.load(body);
+        request('http://boxrec.com/en/boxer/' + id, function (error, response, body) {
+            //console.log('error:', error);
+            //console.log('statusCode:', response && response.statusCode);
+            //console.log('body:', body);
 
-        var boxer = {
-            alias: '',
-            bouts: '',
-        };
+            // Load the page HTML into Cheerio
+            var $ = cheerio.load(body);
 
-        $('.profileTable .rowTable .rowLabel b').filter(function() {
-            if($(this).text().indexOf('alias') > -1) {
-                boxer.alias = $(this).parent().next().text().trim();
-            }
-            if($(this).text().indexOf('bouts') > -1) {
-                boxer.bouts = parseInt($(this).parent().next().text().trim());
-            }
-            if($(this).text().indexOf('debut') > -1) {
-                boxer.debut = $(this).parent().next().text().trim();
-            }
-            if($(this).text().indexOf('nationality') > -1) {
-                boxer.nationality = $(this).parent().next().text().trim();
-            }
-            if($(this).text().indexOf('KOs') > -1) {
-                boxer.knockoutPercentage = parseInt($(this).parent().next().text());
-            }
-            if($(this).text().indexOf('rounds') > -1) {
-                boxer.rounds = parseInt($(this).parent().next().text());
-            }
-            if($(this).text().indexOf('stance') > -1) {
-                boxer.stance = $(this).parent().next().text().trim();
-            }
-            if($(this).text().indexOf('division') > -1) {
-                boxer.weightDivision = $(this).parent().next().text().trim();
-            }
+            var boxer = {
+                alias: '',
+                bouts: '',
+            };
+
+            $('.profileTable .rowTable .rowLabel b').filter(function() {
+                if($(this).text().indexOf('alias') > -1) {
+                    boxer.alias = $(this).parent().next().text().trim();
+                }
+                if($(this).text().indexOf('bouts') > -1) {
+                    boxer.bouts = parseInt($(this).parent().next().text().trim());
+                }
+                if($(this).text().indexOf('debut') > -1) {
+                    boxer.debut = $(this).parent().next().text().trim();
+                }
+                if($(this).text().indexOf('nationality') > -1) {
+                    boxer.nationality = $(this).parent().next().text().trim();
+                }
+                if($(this).text().indexOf('KOs') > -1) {
+                    boxer.knockoutPercentage = parseInt($(this).parent().next().text());
+                }
+                if($(this).text().indexOf('rounds') > -1) {
+                    boxer.rounds = parseInt($(this).parent().next().text());
+                }
+                if($(this).text().indexOf('stance') > -1) {
+                    boxer.stance = $(this).parent().next().text().trim();
+                }
+                if($(this).text().indexOf('division') > -1) {
+                    boxer.weightDivision = $(this).parent().next().text().trim();
+                }
+            });
+
+            // Extract the data we need
+            boxer.dob = $('span[itemprop="birthDate"]').text(),
+            boxer.name = $('h1').text();
+            boxer.wins = $('.profileWLD .bgW').text(),
+            boxer.winsKO = parseInt($('.profileWLD .textWon').text()),
+            boxer.draws = $('.profileWLD .bgD').text(),
+            boxer.losses = $('.profileWLD .bgL').text(),
+            boxer.lossesKO = parseInt($('.profileWLD .textLost').text()),
+
+            console.log(boxer);
+            return boxer;
         });
+    }
+}
 
-        // Extract the data we need
-        boxer.dob = $('span[itemprop="birthDate"]').text(),
-        boxer.name = $('h1').text();
-        boxer.wins = $('.profileWLD .bgW').text(),
-        boxer.winsKO = parseInt($('.profileWLD .textWon').text()),
-        boxer.draws = $('.profileWLD .bgD').text(),
-        boxer.losses = $('.profileWLD .bgL').text(),
-        boxer.lossesKO = parseInt($('.profileWLD .textLost').text()),
+boxrecClient.getBoxerById('356831');
 
-        console.log(boxer);
-        return boxer;
-    });
-};
-
-getBoxerById('356831');
-
-module.exports = getBoxerById;
+module.exports = boxrecClient;
